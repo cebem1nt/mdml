f8224ff7f6d2c061bf4afb7c1c146384e1d579e8
cebem1nt <mineewarik@gmail.com>
2026-03-17 23:01:36 -0300

    Restructuring & refactoring

--- src/mdml.c
+++ src/mdml.c
@@ -16,17 +16,8 @@
  * one will come somewhere in the future 
  */
 
-#include <ctype.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <string.h>
-
 #define BACE_IMPLEMENTATION
-#include "include/bace.h"
+#include "include/bace.h" // IWYU pragma: keep
 
 #define STACK_DEPTH 100
 #define TOKEN_IS_PRIMARY(t) ((t) == TOKEN_H1 || \
@@ -40,6 +31,8 @@
 
 typedef enum {
     TOKEN_UNKNOWN = -1,
+    TOKEN_NEWLINE,
+    TOKEN_TEXT,
     TOKEN_H1,
     TOKEN_H2,
     TOKEN_H3,
@@ -48,12 +41,10 @@ typedef enum {
     TOKEN_H6,
     TOKEN_OL,
     TOKEN_UL,
-    TOKEN_TEXT,
-    TOKEN_NEWLINE,
-} token_type_t;
+} token_kind_t;
 
 typedef struct {
-    token_type_t type;
+    token_kind_t type;
     span_t operand;
     span_t operand_extra;
 } token_t;
@@ -64,30 +55,10 @@ typedef struct {
     size_t len;
 } tokens_arr_t;
 
-static char* readfile(int fd, size_t* out_size) 
-{
-    size_t fsize;
-    char*  out;
-
-    fsize = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);
-
-    if ((out = malloc(fsize)) == NULL)
-        error_abort("%s", "No memory");
-
-    if (read(fd, out, fsize) == -1)
-        perror_abort("read");
-
-    if (out_size)
-        *out_size = fsize;
-
-    return out;
-}
-
 /*
  * Takes in any kind of span and matches it to a token
  */
-token_type_t mdml_span_tokenize(span_t raw) 
+token_kind_t mdml_span_tokenize(span_t raw) 
 {
     if (span_iseq(raw, SPAN("#"))) 
         return TOKEN_H1;
@@ -116,87 +87,35 @@ token_type_t mdml_span_tokenize(span_t raw)
     return TOKEN_TEXT;
 }
 
-const char* mdml_token_to_html(token_type_t t) 
+const char* mdml_token_to_html(token_kind_t tk) 
 {
-    switch (t) {
+    switch (tk) {
+        case TOKEN_NEWLINE: return "br";
+        case TOKEN_TEXT:    return "p";
         case TOKEN_H1:      return "h1";
         case TOKEN_H2:      return "h2";
         case TOKEN_H3:      return "h3";
         case TOKEN_H4:      return "h4";
         case TOKEN_H5:      return "h5";
         case TOKEN_H6:      return "h6";
-        case TOKEN_TEXT:    return "p";
-        case TOKEN_NEWLINE: return "br";
         case TOKEN_OL:      return "ol";
         case TOKEN_UL:      return "ul";
         default:            return "div";
     }
 }
 
-int mdml_convert(tokens_arr_t* tokens) 
+int mdml_lex(span_t stream, tokens_arr_t* dest) 
 {
-    token_type_t stack[STACK_DEPTH];
-    size_t       stack_top = 0;
+    while (stream.length != 0) {
+        span_t line = span_chop_by(&stream, '\n');
 
-    for (size_t i = 0; i < tokens->len; i++) {
-        token_t t = tokens->arr[i];
-        const char* html = mdml_token_to_html(t.type);
-
-        if (t.type == TOKEN_OL || t.type == TOKEN_UL) {            
-            if (stack_top == 0 || stack[stack_top-1] != t.type) {
-                stack[stack_top++] = t.type;
-                printf("<%s>\n", html);
-            } 
-
-            printf("    <li>%.*s</li>\n", (int)t.operand.length, t.operand.ptr);
-        } else if (stack_top > 0) {
-            token_type_t mode = stack[--stack_top];
-            const char* closing = mdml_token_to_html(mode);
-            printf("</%s>\n", closing);
-        } else if (t.type == TOKEN_NEWLINE) {
-            printf("<%s>\n", html);
-        } else {
-            printf("<%s>%.*s</%s>\n", html, 
-                (int)t.operand.length, t.operand.ptr, 
-            html);
-        }
-    }
-
-    if (stack_top > 0) {
-        token_type_t mode = stack[--stack_top];
-        const char* closing = mdml_token_to_html(mode);
-        printf("</%s>\n", closing);
-    }
-
-    return 0;
-}
-
-int mdml_parse(const char* input) 
-{
-    tokens_arr_t  tokens;
-    span_t        line, raw;
-    int           input_fd;
-    size_t        input_size;
-    char*         input_buf;
-
-    if ((input_fd = open(input, O_RDONLY)) == -1)
-        perror_abort("open");
-
-    DA_INIT(&tokens, token_t, 20);
-    
-    input_buf = readfile(input_fd, &input_size);
-    raw = span_from_cstr(input_buf);
-
-    while (raw.length != 0) {
-        line = span_chop_by(&raw, '\n');
-
-        token_type_t token_type = {0};
+        token_kind_t kind;
         span_t       token_raw  = {0},
                      op_extra   = SPAN_EMPTY,
                      op         = SPAN_EMPTY;
         
         if (line.length == 0) {
-            token_type = TOKEN_NEWLINE;
+            kind = TOKEN_NEWLINE;
             goto next;
         }
 
@@ -209,26 +128,53 @@ int mdml_parse(const char* input)
             token_raw.length++;
         }
 
-        token_type = mdml_span_tokenize(token_raw);
+        kind = mdml_span_tokenize(token_raw);
 
-        if (token_type == TOKEN_TEXT) {
+        if (kind == TOKEN_TEXT) {
             op.ptr = line.ptr;
             op.length = line.length;
         }
         
-        else if (TOKEN_IS_PRIMARY(token_type)) {
-            // HTML won't render trailing and leading whitespaces, no need to care
+        else if (TOKEN_IS_PRIMARY(kind)) {
             op.ptr = line.ptr + token_raw.length;
             op.length = line.length - token_raw.length;
+            span_ltrim(&op);
         }
 
 next:
-        DA_APPEND(&tokens, (token_t){token_type, op, op_extra});
+        DA_APPEND(dest, (token_t){kind, op, op_extra});
     }
 
-    mdml_convert(&tokens);
-    free(input_buf);
+    return 0;
+}
 
+int mdml_convert(tokens_arr_t* tokens) 
+{
+    token_kind_t stack[STACK_DEPTH];
+    size_t       stack_top = 0;
+
+    printf("TODO: mdml_convert\n");
+
+    return 0;
+}
+
+int mdml_parse(const char* input) 
+{
+    tokens_arr_t tokens;
+    span_t       stream;
+    size_t       input_size;
+    char*        input_buf;
+
+    DA_INIT(&tokens, token_t, 20);
+    
+    if ((input_buf = readfile(input, &input_size)) == NULL)
+        error_exit("Could not read file\n");
+
+    stream = span_from_cstr(input_buf);
+    mdml_lex(stream, &tokens);
+    mdml_convert(&tokens);
+
+    free(input_buf);
     return 0;
 }
 
